9c4bd42f13118c0abcc2c4fe1cf96277b1a2d044
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2022-11-14 16:09:13 +0530

    Initial Commit

---
+++ InnateMC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InnateMC
+//
+//  Created by Shrish Deshpande on 11/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InnateMC/InnateMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnateMCApp.swift
+//  InnateMC
+//
+//  Created by Shrish Deshpande on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct InnateMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
