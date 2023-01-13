8a4553b9b6d87d2ca75b8d4c5be9450813559024
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-01-13 23:08:55 +0530

    Allow getting available java versions

---
+++ InnateKitTests/JavaTests.swift
@@ -0,0 +1,38 @@
+//
+// Copyright © 2022 Shrish Deshpande
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import XCTest
+import InnateKit
+
+class JavaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func getJavaVersions() throws {
+        self.measure {
+            try! JavaFetch.getAvailableJavaVersions()
+        }
+        let versions = try JavaFetch.getAvailableJavaVersions()
+        XCTAssert(versions.count >= 1)
+    }
+}
