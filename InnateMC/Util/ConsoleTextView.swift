3e19779a269153a63e9ee87af9dae00179fd7a95
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-04-14 12:59:42 +0530

    Tinker with some multiline selectability

--- InnateMC/Util/ConsoleTextView.swift
+++ InnateMC/Util/ConsoleTextView.swift
@@ -19,36 +19,50 @@ import SwiftUI
 
 import SwiftUI
 
-struct ConsoleText: NSViewRepresentable {
+struct ConsoleTextView: NSViewRepresentable {
     typealias NSViewType = NSTextView
     
     var text: String
+    var layoutManager: NSLayoutManager
+    var textContainer: NSTextContainer
     var font: NSFont = NSFont.monospacedSystemFont(ofSize: NSFont.systemFontSize, weight: .regular)
     
     func makeNSView(context: Context) -> NSTextView {
         let textView = NSTextView()
+        textView.minSize = NSSize(width: 200, height: 50)
         textView.isEditable = false
         textView.isSelectable = true
         textView.drawsBackground = false
         textView.font = font
         textView.alignment = NSTextAlignment.natural
         textView.string = text
+        textView.allowsUndo = false
+        textView.textContainer = self.textContainer
         return textView
     }
-
+    
     func updateNSView(_ nsView: NSTextView, context: Context) {
+        self.textContainer.layoutManager = self.layoutManager
         nsView.string = text
         nsView.font = font
+        nsView.textContainer = self.textContainer
     }
 }
 
-struct ConsoleText_Previews: PreviewProvider {
+struct ConsoleTextView_Previews: PreviewProvider {
     static var ints = Array(1...10)
+    static let layoutManager = NSLayoutManager()
+    static let textContainer: NSTextContainer = {
+        var cont = NSTextContainer()
+        layoutManager.addTextContainer(cont)
+        layoutManager.allowsNonContiguousLayout = true
+        return cont
+    }()
 
     static var previews: some View {
-        VStack {
+        VStack(spacing: 0) {
             ForEach(ints, id: \.self) { i in
-                ConsoleText(text: "This is console text \(i)")
+                ConsoleTextView(text: "This is console text \(i)", layoutManager: layoutManager, textContainer: textContainer)
             }
         }
     }
